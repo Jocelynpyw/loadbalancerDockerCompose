337362812f334b9490179bba1a5e21b8e8c6ace9
jocelyn pyw <jocelynwotcheu2@gmail.com>
2024-01-16 14:19:21 +0100

    second version completed with the docker compose det up

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 
 const app = express();
+const server = process.env.server || "GateWay";
 
 app.get("/", (req, res) => {
-  res.send("This is the server's response...");
+  res.send(`This is the response from the server ${server} `);
 });
 app.listen(5050, () => {
   console.log("Listening...");
